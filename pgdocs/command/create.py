386232d12a7a4ba75645fb82ba52407ddede88fa
Boris Korogvich <b.korogvich@gmail.com>
2019-04-07 15:51:27 +0300

    refactor create command

--- pgdocs/command/create.py
+++ pgdocs/command/create.py
@@ -1,3 +1,41 @@
 """
-Create docs in one of the formats
+Create docs in one of the specified formats
 """
+
+from pgdocs import meta
+import os
+from gen import mdgen
+from gen import htmlgen
+from gen import mkdocsgen
+from gen import pdfgen
+
+
+def execute(host, port, db_name, format, output):
+    def out_path(out_dir, extension):
+        filename = "docs.{}".format(extension)
+        return os.path.join(output, filename) if output else filename
+
+    if not format:
+        format = "markdown"
+
+    meta_dir = "meta"
+    metadata = meta.fetch(meta_dir, host, port,
+                          db_name) if host and port else meta.fetch(meta_dir)
+
+    # Here we put generated docs
+    if output and (not os.path.exists(output)):
+        os.makedirs(output)
+
+    if format == "markdown":
+        with open(out_path(output, "md"), "w") as outfile:
+            outfile.write(mdgen.generate(metadata))
+    elif format == "html":
+        with open(out_path(output, format)) as outfile:
+            outfile.write(htmlgen.generate(metadata))
+    elif format == "mkdocs":
+        out_path = output if output else "gen-docs"
+        mkdocsgen.generate(metadata, out_path)
+    elif format == "pdf":
+        pdfgen.generate(metadata)
+    else:
+        print("You entered unsupported docs format...")
--- pgdocs/pgdocs.py
+++ pgdocs/pgdocs.py
@@ -1,15 +1,12 @@
 import argparse
 
-from gen import mdgen
-from gen import pdfgen
-from gen import htmlgen
-from gen import mkdocsgen
 import meta
 import os
 from command import enrich
 from profile import profile
 from command import backup
 from command import show
+from command import create
 
 
 def main():
@@ -66,8 +63,8 @@ def main():
     args = parser.parse_args()
 
     if args.command == "create":
-        create_docs(args.host, args.port, args.database,
-                    args.format, args.output)
+        create.execute(args.host, args.port, args.database,
+                       args.format, args.output)
     elif args.command == "backup":
         backup.execute(args.host, args.port, args.database,
                        args.output, args.format)
@@ -79,52 +76,6 @@ def main():
                      args.schema, args.table)
     else:
         print("You entered unsupported command...")
-
-
-def create_docs(host, port, db_name, format, output):
-    """Select default format when format option isn't specified"""
-    if not format:
-        format = "markdown"
-
-    meta_dir = "meta"
-
-    # Getting database metadata
-    metadata = meta.fetch(
-        meta_dir, host, port, db_name) if host and port else meta.fetch(meta_dir)
-
-    # Here we put generated documentation
-    if output and (not os.path.exists(output)):
-        os.makedirs(output)
-
-    # Generated docs depending on selected format
-    if format == "markdown":
-        print("Start generating docs in Markdown format...")
-
-        out_path = os.path.join(
-            output, "docs.md") if output and format else "docs.md"
-
-        markdown = mdgen.generate(metadata)
-        with open(out_path, "w") as outfile:
-            outfile.write(markdown)
-    elif format == "html":
-        print("Start generating docs in HTML format...")
-
-        out_path = os.path.join(
-            output, "docs.html") if output and format else "docs.html"
-
-        markdown = htmlgen.generate(metadata)
-        with open(out_path, "w") as outfile:
-            outfile.write(markdown)
-    elif format == "mkdocs":
-        print("Start generating docs in MkDocs format...")
-
-        out_path = output if output else "gen-docs"
-
-        mkdocsgen.generate(metadata, out_path)
-        # create docs folder and put all generated Markdown files in it
-        # create mkdocs.yml file
-    elif format == "pdf":
-        pdfgen.generate(metadata)
 
 
 if __name__ == "__main__":
